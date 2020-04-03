1fa7d9f23c4d3b6c5909a2a232d2bae7f8c36535
jlankitus <jlankitus@gmail.com>
2020-04-03 16:04:43 -0700

    upgraded stuff!

--- Planet Gravity/Assets/Attractor.cs
+++ Planet Gravity/Assets/Attractor.cs
@@ -39,8 +39,7 @@ public class Attractor : MonoBehaviour {
 		Vector3 direction = rb.position - rbToAttract.position;
 		float distance = direction.magnitude;
 
-		if (distance == 0f)
-			return;
+		if (distance == 0f) return;
 
 		float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
 		Vector3 force = direction.normalized * forceMagnitude;
